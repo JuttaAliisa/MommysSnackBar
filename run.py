4327c2b4b0e782252a1dc87323ef9e9ad936329f
JuttaAliisa Helin <juttahelin@gmail.com>
2024-02-17 18:41:26 +0000

    Create cell_value to offer identification of the product deleted from stock

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ def consumption():
     """
     function for inputtin consumption values
     """
+# Select the appropriate worksheet
+    worksheet = SHEET.worksheet('usage')
+
 # Get user input
     print('When taking a snack from the stock, please insert correct number for the snack')
     user_choice = input("Enter number: ")
@@ -23,30 +26,34 @@ def consumption():
 # Define the values based on user input
     if user_choice == '1':
         values_to_add = [1, 0, 0, 0, 0, 0]
+        cell_value = worksheet.cell(1, 1).value
     elif user_choice == '2':
         values_to_add = [0, 1, 0, 0, 0, 0]
+        cell_value = worksheet.cell(1, 2).value
     elif user_choice == '3':
         values_to_add = [0, 0, 1, 0, 0, 0]
+        cell_value = worksheet.cell(1, 3).value
     elif user_choice == '4':
         values_to_add = [0, 0, 0, 1, 0, 0]
+        cell_value = worksheet.cell(1, 4).value
     elif user_choice == '5':
         values_to_add = [0, 0, 0, 0, 1, 0]
+        cell_value = worksheet.cell(1, 5).value
     elif user_choice == '6':
         values_to_add = [0, 1, 0, 0, 0, 1]
+        cell_value = worksheet.cell(1, 6).value
     else:
         print("Invalid choice. Please enter a number from 1 through 6")
         consumption()
 
-    # Select the appropriate worksheet
-    worksheet = SHEET.worksheet('usage')
 
     # Add the values to the next available row
     next_row = len(worksheet.get_all_values()) + 1
     worksheet.append_row(values_to_add)
 
-    print(f"Thank you! 1pc deleted from SnackBar stock ")
+    print(f"Thank you! 1pc of {cell_value} deleted from SnackBar stock ")
     print('Do you want to take out another item?')
-    user_choice = input("y=yes, n=no")
+    user_choice = input("y=yes, n=no: ")
     if user_choice == 'y':
         consumption()
     elif user_choice == 'n':
