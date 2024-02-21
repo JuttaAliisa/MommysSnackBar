4b9aa33a3072d81b25541cd895133357dca09031
JuttaAliisa Helin <juttahelin@gmail.com>
2024-02-21 10:08:26 +0000

    Run through linter and fix whitespace and linelength issues

--- run.py
+++ run.py
@@ -15,17 +15,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SPREADSHEET_NAME = 'mommys_snackbar_helper'
 
+
 def clear():
-    '''
+    """
     Clears terminal
-    '''
+    """
     system('cls' if name == 'nt' else 'clear')
+
 
 def consumption(usage_sheet, stock_sheet):
     """
     function for inputting consumption values
     """
-    #clear terminal for better readability
+    # Clear terminal for better readability
     clear()
 
     # Get user input
@@ -33,9 +35,10 @@ def consumption(usage_sheet, stock_sheet):
     print(Style.BRIGHT + "Snack Consumption logging \n")
     print(Style.RESET_ALL)
     print(Fore.BLUE)
-    print('When taking a snack from the stock, please insert correct number for the snack')
+    print('When taking a snack from the stock')
+    print('please insert correct number for the snack')
     print(Style.RESET_ALL)
-    
+
     products = usage_sheet.row_values(1)
     numbers = [1, 2, 3, 4, 5, 6]
     print(Fore.GREEN)
@@ -72,7 +75,6 @@ def consumption(usage_sheet, stock_sheet):
         input()
         consumption(spreadsheet.worksheet('usage'))
 
-
     # Add the values to the next available row to usage sheet
     usage_sheet.append_row(values_to_add)
     # Turn the value negative and add to stock sheet as a new row
@@ -88,13 +90,13 @@ def consumption(usage_sheet, stock_sheet):
         user_choice = input("y=yes, n=no: \n")
         return user_choice.lower()
 
-
     while True:
         user_choice = get_user_choice()
 
         if user_choice == 'y':
-            consumption(spreadsheet.worksheet('usage'), spreadsheet.worksheet('stock'))
-            break 
+            consumption(spreadsheet.worksheet('usage'),
+                        spreadsheet.worksheet('stock'))
+            break
         elif user_choice == 'n':
             print("Thank you for your contribution!\n")
             print("Press enter to get back to start\n")
@@ -106,17 +108,18 @@ def consumption(usage_sheet, stock_sheet):
             print("Invalid choice. Please enter y or n: ")
             print(Style.RESET_ALL)
 
+
 def stock(worksheet):
     """
     Function to check current stock value
     """
-    #clear terminal for better readability
+    # clear terminal for better readability
     clear()
     print(Fore.MAGENTA)
     print(Style.BRIGHT + "The current stock is:")
     print(Style.RESET_ALL)
     print(Fore.GREEN)
-    #print user friendly stock values
+    # print user friendly stock values
     products = worksheet.row_values(1)
     amounts = worksheet.row_values(2)
     for product, amount in zip(products, amounts):
@@ -126,37 +129,40 @@ def stock(worksheet):
     print("\nPress enter to go back to main page")
     input()
     start()
-        
+
+
 def recommendation(usage_sheet, stock_sheet, recommendation_sheet):
     """
     Function to calculate purchase recommendation.
     This is based on usage compared to most recent restock
     The number will change based on usage and size of restock.
     """
-    #clear terminal for better readability
+    # Clear terminal for better readability
     clear()
     print(Fore.MAGENTA)
     print(Style.BRIGHT + "Restock recommendations for a shopping trip \n")
     print(Style.RESET_ALL)
-    print("Please consider purchasing the following snacks to make sure the stock lasts:")
+    print("Please consider purchasing the following")
+    print("snacks to make sure the stock lasts:")
     print(Fore.GREEN)
-    
+
     # Get the values from the sheets
     usage_values = usage_sheet.row_values(2)
     stock_values = stock_sheet.row_values(2)
-    
+
     # Convert the values to integers
     usage_values = [int(value) for value in usage_values]
     stock_values = [int(value) for value in stock_values]
 
     # Calculate the difference between usage and stock
     # If stock has gone to 0, we will add one to cover increasing demand
-    recommendation_values = [usage - stock + 1 if stock == 0 else usage - stock for usage, stock in zip(usage_values, stock_values)]
+    recommendation_values = [usage - stock + 1 if stock == 0 else usage - stock
+                             for usage, stock in zip(usage_values, stock_values)]
 
     # Add the values to the next available row
     recommendation_sheet.update([recommendation_values], 'A2')
 
-    #print reader friendly recommendation list
+    # Print reader friendly recommendation list
     products = recommendation_sheet.row_values(1)
     amounts = recommendation_values
     for product, amount in zip(products, amounts):
@@ -169,11 +175,12 @@ def recommendation(usage_sheet, stock_sheet, recommendation_sheet):
     input()
     start()
 
+
 def restock(restock_sheet, usage_sheet, stock_sheet):
     """
     Function to perform restock of the snack bar
     """
-    #clear terminal for better readability
+    # Clear terminal for better readability
     clear()
     print(Fore.MAGENTA)
     print(Style.BRIGHT + "Welcome to restock feature!")
@@ -218,7 +225,7 @@ def restock(restock_sheet, usage_sheet, stock_sheet):
             for j in range(2, len(all_usage_values)):
                 if all_usage_values[j][i].replace('.', '', 1).isdigit():
                     usage_sheet.update_cell(j + 1, i + 1, '')
-        
+
         restock_values.append(quantity)
 
     # Update the cells with the new values
@@ -230,6 +237,7 @@ def restock(restock_sheet, usage_sheet, stock_sheet):
     print("Press enter to go back to main page")
     input()
     start()
+
 
 def start():
     clear()
@@ -257,37 +265,45 @@ def start():
         if user_choice == '1':
             consumption(spreadsheet.worksheet('usage'), spreadsheet.worksheet('stock'))
         elif user_choice == '2':
-            restock(spreadsheet.worksheet('restock'), spreadsheet.worksheet('usage'), spreadsheet.worksheet('stock'))
+            restock(spreadsheet.worksheet('restock'),
+                    spreadsheet.worksheet('usage'),
+                    spreadsheet.worksheet('stock'))
         elif user_choice == '3':
             stock(spreadsheet.worksheet('stock'))
         elif user_choice == '4':
-            recommendation(spreadsheet.worksheet('usage'), spreadsheet.worksheet('stock'), spreadsheet.worksheet('recommendation'))
+            recommendation(spreadsheet.worksheet('usage'),
+                           spreadsheet.worksheet('stock'),
+                           spreadsheet.worksheet('recommendation'))
         elif user_choice == '5':
             stocktaking(spreadsheet)
         else:
             print("Invalid choice. Please enter a number between 1 and 5")
             continue
 
-        break  # Exit the loop 
+        break  # Exit the loop
+
 
 def stocktaking(spreadsheet):
     """
-    Stocktaking gives opportunity to reset the stock and usage in case the user feels like the numbers are not correct. This feature will delete usage, stock and
-     previous restock data. It gives a clean start for the stock
+    Stocktaking gives opportunity to reset the stock and usage in
+    case the user feels like the numbers are not correct.
+    This feature will delete usage, stock and
+    previous restock data. It gives a clean start for the stock
     """
-    #clear terminal for better readability
+    # Clear terminal for better readability
     clear()
 
     print("Welcome to stocktaking\n")
-    print("If you feel or know that your stock is not accurate, stocktaking will help you")
-    print("Please note that if you proceed with stocktaking, your stock history will be deleted")
-    print("This will affect the recommendations temporarily")
+    print("If you feel or know that your stock is not accurate,")
+    print("stocktaking will help you\n")
+    print("Please note that if you proceed with stocktaking,")
+    print("your stock history will be deleted")
+    print("This will affect the recommendations temporarily\n")
     print("Want to proceed with stock taking?")
 
     def get_user_choice():
         user_choice = input("y=yes, n=no: \n")
         return user_choice.lower()
-
 
     while True:
         user_choice = get_user_choice()
@@ -308,9 +324,12 @@ def stocktaking(spreadsheet):
             # Clear everything from the restock sheet starting at row 2
             spreadsheet.worksheet('restock').batch_clear([restock_range])
 
-            print("Stockdata has been cleared.\n Please proceed to restocking the actual stock you have by pressing enter")
+            print("Stockdata has been cleared.")
+            print("Please proceed to restocking the actual stock you have by pressing enter")
             input()
-            restock(spreadsheet.worksheet('restock'), spreadsheet.worksheet('usage'), spreadsheet.worksheet('stock'))
+            restock(spreadsheet.worksheet('restock'),
+                    spreadsheet.worksheet('usage'),
+                    spreadsheet.worksheet('stock'))
             break  # exit the loop
         elif user_choice == 'n':
             print("Stocktaking cancelled")
@@ -322,5 +341,6 @@ def stocktaking(spreadsheet):
             print(Fore.RED)
             print("Invalid choice. Please enter y or n: ")
             print(Style.RESET_ALL)
+
 
 start()
